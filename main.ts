8436ee821d26bc2198aa52067a2adc1f152cbdd8
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2025-10-29 11:48:20 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,13 @@
 basic.forever(function () {
+    let Index = 0
     for (let Index = 0; Index <= 2; Index++) {
         led.plot(Index, 2)
         led.plot(Index + 2, 2)
         basic.pause(500)
         basic.clearScreen()
     }
-    for (let Index = 0; Index <= 1; Index++) {
-        led.plot(3 - Index, 2)
-        led.plot(3 - Index - 2, 2)
-        basic.pause(500)
-        basic.clearScreen()
-    }
+    led.plot(3 - Index, 2)
+    led.plot(3 - Index - 2, 2)
+    basic.pause(500)
+    basic.clearScreen()
 })
